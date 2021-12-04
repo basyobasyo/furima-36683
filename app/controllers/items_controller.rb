307f5707477e422dc6c923b8e56f1af540cefea6
basyobasyo <om1997518@gmail.com>
2021-12-04 10:50:33 +0900

    Itemモデルの作成、新規出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+
+  before_action :move_log_in, only: [:new]
+
   def index
   end
+
+  def new
+  end
+
+  private
+
+  def move_log_in
+    unless user_signed_in?
+      render("devise/sessions/new")
+    end
+  end
+
 end

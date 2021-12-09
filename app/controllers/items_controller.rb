d78e4cb534cf878c24c500e5b55a480e6cc68be5
basyobasyo <om1997518@gmail.com>
2021-12-09 20:39:15 +0900

    商品情報の登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
@@ -14,6 +24,10 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       render("devise/sessions/new")
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name,:item_explanation, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  has_one_attached :image
   
   validates :category_id, numericality: { other_than: 1 } 
 

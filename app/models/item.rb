b6ec8b6aaabfa399d600049b66e857121f29abb9
basyobasyo <om1997518@gmail.com>
2021-12-10 18:21:57 +0900

    itemモデルの編集とテストコード記述の準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-
   has_one_attached :image
   
   validates :item_name       , presence: true
@@ -9,26 +8,13 @@ class Item < ApplicationRecord
   validates :price           , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :image           , presence: true
 
-  validates :category_id, numericality: { other_than: 1 } 
+  validates :category_id    , numericality: { other_than: 1 } 
   validates :shipping_fee_id, numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :status_id      , numericality: { other_than: 1 } 
+  validates :prefecture_id  , numericality: { other_than: 1 } 
   validates :days_to_ship_id, numericality: { other_than: 1 } 
 
-
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :shippingfee
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :status
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :daystoship
+  belongs_to :category, :shippingfee, :status, :prefecture, :daystoship
 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name           {Faker::Name.initials(number: 3)}
+    item_explanation    {Faker::Lorem.sentence}
+    price               {Faker::Number.between(from: 300, to: 9999999)}
+    category_id         {Faker::Number.between(from: 2,   to: 11)}
+    status_id           {Faker::Number.between(from: 2,   to: 7)}
+    shipping_fee_id     {Faker::Number.between(from: 2,   to: 3)}
+    prefecture_id       {Faker::Number.between(from: 2,   to: 48)}
+    days_to_ship_id     {Faker::Number.between(from: 2,   to: 4)}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end

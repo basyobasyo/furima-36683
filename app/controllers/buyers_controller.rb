5d53f533f18a1d8c1836a09c3dc07d65640a1e1c
basyobasyo <om1997518@gmail.com>
2021-12-28 06:53:14 +0900

    saveメソッドの追加、コントローラーの編集

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,6 +2,21 @@ class BuyersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @buyer_address = BuyerAddress.new(buyer_params)
   end
 
+  def create
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer_address).permit(:postal_code, :prefecture, :municipalities, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -3,5 +3,10 @@ class BuyerAddress
   attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building, :telephone_number, :user_id, :item_id, :buyer_id
 
   # ここにバリデーションの記述を行う
+  
+  def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture: prefecture, municipalities: municipalities, address: address, building: building, telephone_number: telephone_number, buyer_id: buyer.id)
+  end
 
 end

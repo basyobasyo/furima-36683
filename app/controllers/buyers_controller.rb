768356051614a80bd9b082665d9bf0dd6ceab930
basyobasyo <om1997518@gmail.com>
2022-01-01 13:43:10 +0900

    詳細ページのsold out表示、ログアウト状態で購入ページに遷移できない設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,10 +1,16 @@
 class BuyersController < ApplicationController
+  before_action :authenticate_user!, only: :index
 
   def index
     @item = Item.find(params[:item_id])
       if current_user == @item.user
         redirect_to root_path
       end
+
+      if Buyer.exists?(item_id: @item.id)
+        redirect_to root_path
+      end
+
     @buyer_address = BuyerAddress.new
   end
 
